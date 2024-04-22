5354cc33dec430033fc7b230ccf2ed10fed34854
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2024-04-22 14:45:21 +0100

    feat: added edit modal

--- src/pages/dashboard/admin/transactions/index.tsx
+++ src/pages/dashboard/admin/transactions/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import DashboardLayout from '@/components/layout/dashboard/DashboardLayout'
+import DashboardSidebar from '@/components/layout/dashboard/DashboardSidebar'
 
 const Transactions = () => {
   return (
-    <div>Transactions</div>
+    <DashboardSidebar>
+      <div className="w-full mt-10 px-8">
+        <h1 className="text-2xl font-semibold py-8">Transactions</h1>
+      </div>
+    </DashboardSidebar>
   )
 }
 
 export default Transactions
+
+Transactions.getLayout = function getLayout(page: React.ReactElement) {
+  return <DashboardLayout page={"transactions"} >{page}</DashboardLayout>;
+};
