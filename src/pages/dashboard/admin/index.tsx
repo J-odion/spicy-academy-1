dc4faab541e4e2263f01bfacb9b311bc76fe1fd0
Benedicta <buonyebuchi@student.oauife.edu.ng>
2024-04-26 08:24:36 +0100

    Filled empty admin component

---
+++ src/pages/dashboard/admin/index.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import DashboardSidebar from '@/components/layout/dashboard/DashboardSidebar'
+
+const Admin: NextPageWithLayout = () => {
+
+  return (
+    <DashboardSidebar>
+      <div className="w-full md:mt-20">
+     
+        
+      </div>
+      
+    </DashboardSidebar>
+  )
+}
+
+export default Admin
+
