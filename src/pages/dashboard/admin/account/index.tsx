4efedeb16e7d1959267f1072bdc2d991b6200d72
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2024-04-27 10:28:15 +0100

    fix:added responsiveness to dashboard

--- src/pages/dashboard/admin/account/index.tsx
+++ src/pages/dashboard/admin/account/index.tsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
-import { NextPageWithLayout } from '@/pages/_app';
-import DashboardSidebar from '@/components/layout/dashboard/DashboardSidebar';
-import DashboardLayout from '@/components/layout/dashboard/DashboardLayout';
-import { Button } from '@/components/ui/button';
+import { useState } from "react";
+import { NextPageWithLayout } from "@/pages/_app";
+import DashboardSidebar from "@/components/layout/dashboard/DashboardSidebar";
+import DashboardLayout from "@/components/layout/dashboard/DashboardLayout";
+import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
-import Moment from 'react-moment';
-import { Skeleton } from '@/components/ui/skeleton';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import Image from 'next/image';
-import { data } from '@/components/layout/dashboard/Cards';
-import { BookOpen } from 'lucide-react';
-import { requests, transactions, support } from '@/data/data';
-
-
+import Moment from "react-moment";
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Image from "next/image";
+import { data } from "@/components/layout/dashboard/Cards";
+import { BookOpen } from "lucide-react";
+import { requests, transactions, support } from "@/data/data";
 
 const Dashboard: NextPageWithLayout = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -20,35 +24,51 @@ const Dashboard: NextPageWithLayout = () => {
   const [showAllTutorialRequests, setShowAllTutorialRequests] = useState(false);
   const [showAllSupport, setShowAllSupport] = useState(false);
 
-  const recentTransactions = showAllTransactions ? transactions : transactions.slice(0, 4);
-  const tutorRequests = showAllTutorialRequests ? requests : requests.slice(0, 4);
+  const recentTransactions = showAllTransactions
+    ? transactions
+    : transactions.slice(0, 4);
+  const tutorRequests = showAllTutorialRequests
+    ? requests
+    : requests.slice(0, 4);
   const supports = showAllSupport ? support : support.slice(0, 4);
 
   const toggleShowTransactions = () => {
-    setShowAllTransactions(prev => !prev);
+    setShowAllTransactions((prev) => !prev);
   };
 
   const toggleShowTutorialRequests = () => {
-    setShowAllTutorialRequests(prev => !prev);
-  }
+    setShowAllTutorialRequests((prev) => !prev);
+  };
 
   const toggleShowSupport = () => {
-    setShowAllSupport(prev => !prev);
-  }
+    setShowAllSupport((prev) => !prev);
+  };
 
   return (
     <DashboardSidebar>
-      <div className="w-full md:mt-20">
+      <div className="w-full mt-20 md:mt-20">
         <div className="items-center justify-between md:flex gap-10">
-          <div className='flex flex-col'>
+          <div className="flex flex-col">
             <div>
-              <div className="grid grid-cols-3 gap-8">
+              <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {data.map((item) => (
-                  <div key={item.id} className="flex w-[200px] h-[250px] items-center justify-start p-4 rounded-lg shadow-md relative overflow-hidden" style={{ backgroundColor: item.color }}>
-                    <Image src='/images/blob.svg' alt="dashboard" width={150} height={100} className='absolute -top-3 right-0' />
-                    <div className="flex flex-col gap-4">
-                      <div className="w-8 h-8"><BookOpen size={20} /></div>
-                      <h6 className="ml-4 text-lg font-semibold">{item.text}</h6>
+                  <div
+                    key={item.id}
+                    className="flex flex-col w-full h-[250px] p-4 rounded-lg shadow-md relative overflow-hidden"
+                    style={{ backgroundColor: item.color }}
+                  >
+                    <Image
+                      src="/images/blob.svg"
+                      alt="dashboard"
+                      width={150}
+                      height={100}
+                      className="absolute -top-3 right-0"
+                    />
+                    <div className="flex flex-col flex-grow justify-center items-center gap-4">
+                      <div className="w-8 h-8">
+                        <BookOpen size={20} />
+                      </div>
+                      <h6 className="text-lg font-semibold">{item.text}</h6>
                       <h1 className="text-3xl font-semibold">{item.value}</h1>
                     </div>
                   </div>
@@ -59,10 +79,14 @@ const Dashboard: NextPageWithLayout = () => {
             <div>
               {!isLoading ? (
                 <div className="my-8 bg-[#FEF9F8] p-8 border-[#C4AAA1] border-2 rounded-md">
-                  <div className='flex justify-between items-center'>
+                  <div className="flex justify-between items-center">
                     <p>Recent transactions</p>
-                    <Button variant={'link'} className='text-[#A85334]' onClick={toggleShowTransactions}>
-                      {showAllTransactions ? 'View less' : 'View all'}
+                    <Button
+                      variant={"link"}
+                      className="text-[#A85334]"
+                      onClick={toggleShowTransactions}
+                    >
+                      {showAllTransactions ? "View less" : "View all"}
                     </Button>
                   </div>
                   <Table>
@@ -79,13 +103,20 @@ const Dashboard: NextPageWithLayout = () => {
                           </TableCell>
                           <TableCell>
                             <Select>
-                              <SelectTrigger className='border-2 border-[#F8DEBD] rounded-md'>
-                                <SelectValue placeholder={`${transaction.status}`} className={transaction.status === 'Success' ? 'placeholder:text-[#E1B57C]' : 'placeholder:text-red-500'}></SelectValue>
+                              <SelectTrigger className="border-2 border-[#F8DEBD] rounded-md">
+                                <SelectValue
+                                  placeholder={`${transaction.status}`}
+                                  className={
+                                    transaction.status === "Success"
+                                      ? "placeholder:text-[#E1B57C]"
+                                      : "placeholder:text-red-500"
+                                  }
+                                ></SelectValue>
                               </SelectTrigger>
                               <SelectContent>
-                                <SelectItem value='success'>Success</SelectItem>
-                                <SelectItem value='pending'>Pending</SelectItem>
-                                <SelectItem value='failed'>Failed</SelectItem>
+                                <SelectItem value="success">Success</SelectItem>
+                                <SelectItem value="pending">Pending</SelectItem>
+                                <SelectItem value="failed">Failed</SelectItem>
                               </SelectContent>
                             </Select>
                           </TableCell>
@@ -104,16 +135,18 @@ const Dashboard: NextPageWithLayout = () => {
 
           <div className="flex flex-col">
             <div>
-              <Button className='bg-[#A85334] w-full'>
-                + Add tutor
-              </Button>
+              <Button className="bg-[#A85334] w-full">+ Add tutor</Button>
             </div>
 
             <div className="mt-8 bg-[#FEF9F8] p-8 border-[#C4AAA1] border-2 rounded-md">
-              <div className='flex justify-between items-center'>
+              <div className="flex justify-between items-center">
                 <p>Tutor Requests</p>
-                <Button variant={'link'} className='text-[#A85334]' onClick={toggleShowTutorialRequests}>
-                  {showAllTutorialRequests ? 'View less' : 'View all'}
+                <Button
+                  variant={"link"}
+                  className="text-[#A85334]"
+                  onClick={toggleShowTutorialRequests}
+                >
+                  {showAllTutorialRequests ? "View less" : "View all"}
                 </Button>
               </div>
               <Table>
@@ -124,12 +157,19 @@ const Dashboard: NextPageWithLayout = () => {
                       <TableCell>{request.email}</TableCell>
                       <TableCell>
                         <Select>
-                          <SelectTrigger className='border-2 border-[#F8DEBD] rounded-md'>
-                            <SelectValue placeholder={`${request.status}`} className={request.status === 'Accept' ? 'placeholder:text-[#E1B57C]' : 'placeholder:text-red-500'}></SelectValue>
+                          <SelectTrigger className="border-2 border-[#F8DEBD] rounded-md">
+                            <SelectValue
+                              placeholder={`${request.status}`}
+                              className={
+                                request.status === "Accept"
+                                  ? "placeholder:text-[#E1B57C]"
+                                  : "placeholder:text-red-500"
+                              }
+                            ></SelectValue>
                           </SelectTrigger>
                           <SelectContent>
-                            <SelectItem value='accept'>Accept</SelectItem>
-                            <SelectItem value='reject'>Reject</SelectItem>
+                            <SelectItem value="accept">Accept</SelectItem>
+                            <SelectItem value="reject">Reject</SelectItem>
                           </SelectContent>
                         </Select>
                       </TableCell>
@@ -141,10 +181,14 @@ const Dashboard: NextPageWithLayout = () => {
 
             <div>
               <div className="mt-8 bg-[#FEF9F8] p-8 border-[#C4AAA1] border-2 rounded-md">
-                <div className='flex justify-between items-center'>
+                <div className="flex justify-between items-center">
                   <p>Support</p>
-                  <Button variant={'link'} className='text-[#A85334]' onClick={toggleShowSupport}>
-                    {showAllSupport ? 'View less' : 'View all'}
+                  <Button
+                    variant={"link"}
+                    className="text-[#A85334]"
+                    onClick={toggleShowSupport}
+                  >
+                    {showAllSupport ? "View less" : "View all"}
                   </Button>
                 </div>
                 <Table>
@@ -154,7 +198,12 @@ const Dashboard: NextPageWithLayout = () => {
                         <TableCell>{support.name}</TableCell>
                         <TableCell>{support.email}</TableCell>
                         <TableCell>
-                          <Button className='border-[#A85334] text-[#A85334]' variant={'outline'}>View</Button>
+                          <Button
+                            className="border-[#A85334] text-[#A85334]"
+                            variant={"outline"}
+                          >
+                            View
+                          </Button>
                         </TableCell>
                       </TableRow>
                     ))}
@@ -166,11 +215,11 @@ const Dashboard: NextPageWithLayout = () => {
         </div>
       </div>
     </DashboardSidebar>
-  )
-}
+  );
+};
 
 export default Dashboard;
 
 Dashboard.getLayout = function getLayout(page: React.ReactElement) {
-  return <DashboardLayout page={"dashboard"} >{page}</DashboardLayout>;
+  return <DashboardLayout page={"dashboard"}>{page}</DashboardLayout>;
 };
