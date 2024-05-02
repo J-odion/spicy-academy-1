a852362b2ab0426a72931632e92a8c5c39ae97b3
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2024-05-02 05:45:08 +0100

    feat:Login page added

--- src/pages/Auth/login/index.tsx
+++ src/pages/Auth/login/index.tsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import AuthLayout from "@/components/layout/auth/AuthLayout";
+import AuthSection from "@/components/layout/auth/AuthSection";
+import { TypographyH1 } from "@/components/typography";
+import { signInFormSchema } from "@/lib/formSchema";
+import { NextPageWithLayout } from "@/pages/_app";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField } from "@/components/ui/form";
+import FormRender from "@/components/FormRender";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/router";
+import CustomButton from "@/components/CustomButton";
+import { Input } from "@/components/ui/input";
 
-const index = () => {
+const SignIn: NextPageWithLayout = () => {
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof signInFormSchema>>({
+    resolver: zodResolver(signInFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+
+  const onSubmit = (values: z.infer<typeof signInFormSchema>) => {
+    setIsLoading(true);
+    toast({
+      title: "Logged in successfully!",
+      description: "Redirecting to dashboard",
+      className: "text-[#141619] bg-[#d3d3d4] border-[#bcbebf]",
+    })
+    setTimeout(() => {
+      setIsLoading(false);
+      router.push("/dashboard/student");
+    }, 2000);
+  };
+
   return (
-    <div>index</div>
-  )
-}
+    <AuthSection>
+      <TypographyH1 className="mb-4">Hi, create an account to get started</TypographyH1>
+      <p className="">
+            Don't have an account?{" "}
+            <Link href="/auth/signup" className="text-[#A85334]">
+              Sign up
+            </Link>
+          </p>
 
-export default index
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+
+
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormRender
+                label="Email"
+                placeholder="Enter your email"
+                field={field}
+              />
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormRender
+                label="Password"
+                placeholder="Enter your password"
+                field={field}
+                type="password"
+              />
+            )}
+          />
+
+
+          <CustomButton
+            type="submit"
+            className=" bg-[#A85334]"
+            disabled={isLoading}
+            isLoading={isLoading}
+          >
+            Log in
+          </CustomButton>
+
+
+        </form>
+      </Form>
+    </AuthSection>
+  );
+};
+
+export default SignIn;
+
+SignIn.getLayout = function getLayout(page: React.ReactElement) {
+  return <AuthLayout page={"signIn"}>{page}</AuthLayout>;
+};
