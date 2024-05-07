4846622acc1e4040cd48757912bbdea4f6338012
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2024-05-07 17:37:12 +0100

    feat: subscriptions plans added

--- src/pages/dashboard/student/courses/index.tsx
+++ src/pages/dashboard/student/courses/index.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 import DashboardSidebar from '@/components/layout/students_dashboard/DashboardSidebar'
 import DashboardLayout from '@/components/layout/students_dashboard/DashboardLayout'
 import { NextPageWithLayout } from '@/pages/_app'
+import { Button } from '@/components/ui/button'
+import CoursesHeaderTab from '@/components/tabs/student_dashboard/CoursesHeaderTab'
 
 const Courses: NextPageWithLayout = () => {
   return (
-    <DashboardSidebar>Courses</DashboardSidebar>
+    <DashboardSidebar>
+        <div className="w-full mt-20 md:mt-20">
+            <div className='flex justify-between'>
+                <h1>Purchased courses</h1>
+                <Button>Buy course</Button>
+            </div>
+            <CoursesHeaderTab currentTab='all'/>
+
+        </div>
+    </DashboardSidebar>
   )
 }
 
