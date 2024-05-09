c3a8da70a24b64d3fe2d4bcba0f301f81f60cf86
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2024-05-09 21:18:28 +0100

    feat:added curriculum page

--- src/components/tabs/student_dashboard/CurriculumHeaderTab.tsx
+++ src/components/tabs/student_dashboard/CurriculumHeaderTab.tsx
@@ -18,7 +18,7 @@ const CurriculumHeaderTab = ({currentTab='beginner'}: CurriculumProps) => {
             Beginner
           </li>
         </Link>
-        <Link href="/dashboard/student/amateur">
+        <Link href="/dashboard/student/curriculum/amateur">
           <li
             className={`cursor-pointer ${
               currentTab === "amateur" && "border-b-2 border-[#A85334] text-[#A85334] bg-[#FDF4E9] py-2 px-4"
